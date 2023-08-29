189d93f8406e25472e9be597e0d23313f5c5ec4d
RayoCristian <143460474+RAYO-MONTOYA-CRISTIAN@users.noreply.github.com>
2023-08-29 01:46:45 -0500

    Add files via upload

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de minutos:");
+            int totalMinutos = Convert.ToInt32(Console.ReadLine());
+
+            int horas = totalMinutos / 60;
+            int minutos = totalMinutos % 60;
+
+            Console.WriteLine($"{totalMinutos} minutos son {horas} horas y {minutos} minutos.");
+        }
+    }
+}
